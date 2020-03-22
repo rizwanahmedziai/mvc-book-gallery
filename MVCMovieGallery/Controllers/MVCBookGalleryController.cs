182bfb21685cade3fc911d3d8aad00de4fcad66b
Rizwan Ahmed <rizwanahmedziai@gmail.com>
2020-03-22 23:14:06 +0500

    Added an Action to the controller

--- MVCMovieGallery/Controllers/MVCBookGalleryController.cs
+++ MVCMovieGallery/Controllers/MVCBookGalleryController.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MVCMovieGallery.Controllers
+namespace MVCBookGallery.Controllers
 {
     public class MVCBookGalleryController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from book controller";
+        }
     }
 }
