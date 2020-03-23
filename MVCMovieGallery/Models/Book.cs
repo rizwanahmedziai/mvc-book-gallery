a04b2560585580c5a627eab18ad939f98e1c1579
Rizwan Ahmed <rizwanahmedziai@gmail.com>
2020-03-23 11:17:24 +0500

    Added Models

---
+++ MVCMovieGallery/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMovieGallery.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favourite { get; set; }
+    }
+}
